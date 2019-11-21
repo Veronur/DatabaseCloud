9b135bc3d8291a3f3e837baba6bc294917a7beb3
Veronur <luccadelchiaro@gmail.com>
2019-11-21 14:40:01 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pymongo
+from pymongo import MongoClient
+client = MongoClient('localhost', 5000)
+db = client.tarefas
+collection = db.tarefas
+
+
+def adicionaTarefas(nome):
+    tarefa={"Nome": "{}".format(nome)}
+    try:
+        collection.insert_one(tarefa).inserted_id
+    except:
+        print('Falha ao adicionar')
+    
+def listaTarefas():
+    ret=[]
+    try:
+        cursor = collection.find({})
+        for document in cursor:
+            ret.append(document)    
+        return ret
+    except:
+        print('Falha na listagem')
+
+def removeTarefas(nome):
+    try:
+        collection.remove({"Nome": "{}".format(nome)})
+    except:
+        print('Falha na delecao')
