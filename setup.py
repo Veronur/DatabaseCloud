e3d1dc484cc9dba3b090aafb92223cde2b7a10b8
Veronur <luccadelchiaro@gmail.com>
2019-11-21 15:15:08 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ def adicionaTarefas(nome):
     tarefa={"Nome": "{}".format(nome)}
     print(tarefa)
     try:
-        collection.insert_one(tarefa).inserted_id
+        collection.insert_one(tarefa)
     except:
         print('Falha ao adicionar')
     
-def listaTarefas():
+def buscaTarefa(nome):
     ret=[]
     try:
-        cursor = collection.find({})
+        cursor = collection.find_one({"Nome": "{}".format(nome)})
         for document in cursor:
             ret.append(document)    
         return ret
@@ -29,8 +29,8 @@ def removeTarefas(nome):
     except:
         print('Falha na delecao')
 
-print(listaTarefas())
+print(buscaTarefa('asdsad'))
 adicionaTarefas('asdsad')
-print(listaTarefas())
+print(buscaTarefa('asdsad'))
 removeTarefas('asdsad')
-print(listaTarefas())
+print(buscaTarefa('asdsad'))
