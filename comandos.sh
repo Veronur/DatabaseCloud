45e87b662b59205911e45284dd7f93c5bf5444c2
Veronur <luccadelchiaro@gmail.com>
2019-11-22 09:38:05 -0300

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -8,34 +8,6 @@ sudo apt install -y mysql-server mysql-client
 sudo apt install expect -y
 
 
-mysql_secure_installation
-aptitude -y install expect
-
-// Not required in actual script
-MYSQL_ROOT_PASSWORD=senha123
-
-SECURE_MYSQL=$(expect -c "
-set timeout 10
-spawn mysql_secure_installation
-expect \"Enter current password for root (enter for none):\"
-send \"$MYSQL\r\"
-expect \"Change the root password?\"
-send \"n\r\"
-expect \"Remove anonymous users?\"
-send \"y\r\"
-expect \"Disallow root login remotely?\"
-send \"n\r\"
-expect \"Remove test database and access to it?\"
-send \"y\r\"
-expect \"Reload privilege tables now?\"
-send \"y\r\"
-expect eof
-")
-
-echo "$SECURE_MYSQL"
-
-aptitude -y purge expect
-
 mysql -e "DROP DATABASE IF EXISTS Cloud;"
 mysql -e "CREATE DATABASE IF NOT EXISTS Cloud DEFAULT CHARACTER SET utf8 ;"
 mysql -e "USE Cloud ;"
