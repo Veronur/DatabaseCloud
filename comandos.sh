4b80c489c0b1cd4da8857818a82ab2d9b39cfe26
Veronur <luccadelchiaro@gmail.com>
2019-11-22 09:27:42 -0300

    Update comandos.sh

--- comandos.sh
+++ comandos.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)"
 sudo apt -y install libcurl3
-sudo apt-get install -y mssql-server
+sudo apt install -y mysql-server mysql-client
 sudo apt install expect -y
+
+
 mysql_secure_installation
+aptitude -y install expect
 
+// Not required in actual script
+MYSQL_ROOT_PASSWORD=senha123
 
-MYSQL_ROOT_PASSWORD=root
 SECURE_MYSQL=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
@@ -19,17 +24,23 @@ send \"n\r\"
 expect \"Remove anonymous users?\"
 send \"y\r\"
 expect \"Disallow root login remotely?\"
-send \"y\r\"
+send \"n\r\"
 expect \"Remove test database and access to it?\"
 send \"y\r\"
 expect \"Reload privilege tables now?\"
 send \"y\r\"
 expect eof
 ")
+
 echo "$SECURE_MYSQL"
-sudo apt purge expect -y
 
+aptitude -y purge expect
 
+mysql -e "DROP DATABASE IF EXISTS Cloud;"
+mysql -e "CREATE DATABASE IF NOT EXISTS Cloud DEFAULT CHARACTER SET utf8 ;"
+mysql -e "USE Cloud ;"
+mysql -e "DROP TABLE IF EXISTS Cloud.Tarefas ;"
+mysql -e "CREATE TABLE IF NOT EXISTS Cloud.Tarefas (Nome VARCHAR(45) UNIQUE,PRIMARY KEY (Nome))ENGINE = InnoDB;"
 
 
 
