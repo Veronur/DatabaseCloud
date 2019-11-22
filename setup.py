99460c5ab5304be69ce3845f7a24b67d4ef346d5
Veronur <luccadelchiaro@gmail.com>
2019-11-22 11:01:12 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,8 @@
-import pymongo
-from pymongo import MongoClient
-client = MongoClient()
-db = client.tarefas
-collection = db.tarefas
+import pymysql.cursors
 
-
-def adicionaTarefas(nome):
-    tarefa={"Nome": "{}".format(nome)}
-    #print(tarefa)
-    try:
-        collection.insert_one(tarefa)
-    except:
-        print('Falha ao adicionar')
-    
-def buscaTarefa(nome):
-    ret=[]
-    try:
-        cursor = collection.find_one({"Nome": "{}".format(nome)})
-        for document in cursor:
-            ret.append(document)    
-        return ret
-    except:
-        print('Falha na listagem')
-
-def removeTarefas(nome):
-    try:
-        collection.remove({"Nome": "{}".format(nome)})
-    except:
-        print('Falha na delecao')
-
-print(buscaTarefa('asdsad'))
-adicionaTarefas('asdsad')
-print(buscaTarefa('asdsad'))
-removeTarefas('asdsad')
-print(buscaTarefa('asdsad'))
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='root',
+                             db='Cloud',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
